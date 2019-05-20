f06a59bfbff21b7123cea8397d73a0a119efd724
Léa Harabagiu <elessach@gmail.com>
2019-05-20 13:31:10 +0200

    restructured & commented Grid

--- src/fr/upem/captcha/ui/Grid.java
+++ src/fr/upem/captcha/ui/Grid.java
@@ -9,41 +9,44 @@ import java.util.List;
 import java.util.Random;
 
 public class Grid {
-	private int difficulty;
-	private List<URL> images;
-	private String correct = "";
-	private int nbCorrect = 0;
+	private int difficulty;								// difficulté de la grille (nombre de lignes
+	private List<URL> images = new ArrayList<URL>();	// images de la grille
+	private String correct = "";						// nom de la classe à sélectionner
+	private int nbCorrect = 0;							// nombre de photos correcte dans la grille
 	
+	// constructeur par paramètres
 	public Grid(int d) {
 		this.difficulty = d;
-		this.images = new ArrayList<URL>();
 	}
 	
+	// ajout d'une image par son url aux images de la grille
 	public void addImage(URL u) {
 		images.add(u);
 	}
+	// définit le type d'image à sélectionner
 	public void setCorrect() {
 		Random r = new Random();
 		int size = images.size();
-		int i = r.nextInt(size);
-		System.out.println(i);
-		URL correctURL = images.get(i);
-		correct = getClassNameFromURL(correctURL);
+		URL correctURL = images.get(r.nextInt(size));	// on choisit une des images affichées qui va définir le type à sélectionner
+		correct = getClassNameFromURL(correctURL);		// on récuppère le nom de la classe correspondante
 		for(URL url : images) {
-			if(getClassNameFromURL(url).contentEquals(correct)) nbCorrect++;
+			if(getClassNameFromURL(url).contentEquals(correct)) nbCorrect++;	// on compte le nombre d'images de ce type dans la grille
 		}
 	}
 	
+	// définit si les images sélectionnées sont correctes
 	public boolean isCorrect(List<URL> selected) {
 		int n = 0;
 		for(URL url : selected) {
-			if(getClassNameFromURL(url).contentEquals(correct)) {
+			if(getClassNameFromURL(url).contentEquals(correct)) {	// on compte le nombre d'images correcte
 				n++;
 			}
 		}
-		if (n == nbCorrect) return true;
+		if (n == nbCorrect) return true;	// si le nombre correspond alors seulement c'est bon
 		else return false;
 	}
+	
+	// convertit une url en nom de classe correspondant
 	private String getClassNameFromURL(URL url) {
 		URI uri = null;
 		try {
@@ -57,12 +60,15 @@ public class Grid {
 		return parentName.substring(0, 1).toUpperCase() + parentName.substring(1);
 	}
 	
+	// retourne la difficulté
 	public int getDifficulty() {
 		return difficulty;
 	}
+	// retourne le nombre d'images dans la grille
 	public int getImagesNumber() {
 		return difficulty * 3;
 	}
+	// retourne le nom de classe correct
 	public String getCorrect() {
 		return correct;
 	}
